10ec81687a4a80305a5a947652d37c18140b456c
Wayne Fincher <slowlychillin@yahoo.com>
2020-10-02 10:38:08 -0400

    This is a doubly linked list
    Only the basics are implemented. It has insertLast(value), remove(node), size(), first(), and last() methods. The last two methods return a node. Nodes have previous(), next(), and value() methods. There are also setPrevious() and setNext() methods. But these two are intended only for the DoublyLinkedList class to use. Using them well certainly destroy the doubly linked list. All methods operate in the running time expected of a doubly linked list.

---
+++ DoublyLinkedList.php
@@ -0,0 +1,126 @@
+<?php
+/*
+	Created by Wayne Fincher
+	Released to the public domain.
+	No warrenty of mechantability or usefullness is claimed or implied. Use at your own discretion!
+*/
+
+
+
+
+class DoublyLinkedListNode {
+	
+	public function __construct($value, $previous, $next) {
+		$this->value = $value;
+		$this->setPrevious($previous);
+		$this->setNext($next);
+	}
+	
+	private $previous = null;
+	
+	private $next = null;
+	
+	private $value = null;
+	
+	public function previous() {
+		return $this->previous;
+	}
+	
+	public function next() {
+		return $this->next;
+	}
+	
+	public function value() {
+		return $this->value;
+	}
+	
+	public function setPrevious($previous) {
+		$this->previous = $previous;
+	}
+	
+	public function setNext($next) {
+		$this->next = $next;
+	}
+	
+	public function __toString() {
+		return $this->value();
+	}
+}
+
+class DoublyLinkedList {
+	
+	/*
+		Note that we don't need a setFirst() method since $first is controlled internally.
+	*/
+	private $first = null;
+	
+	
+	/*
+		Note that we don't need a setLast() method since $last is controlled internally.
+	*/
+	private $last = null;
+	
+	private $size = 0;
+	
+	public function first() {
+		return $this->first;
+	}
+	
+	public function last() {
+		return $this->last;
+	}
+	
+	public function insertLast($value) {
+		if (is_null($this->last)) {
+			$node = new DoublyLinkedListNode($value, null, null);
+			$this->last = $this->first = $node;
+		} else {
+			$node = new DoublyLinkedListNode($value, $this->last, null);
+			$this->last->setNext($node);
+			$this->last = $node;
+		}
+		$this->size++;
+	}
+	
+	public function removeNode($node) {
+		if ($node == $this->first) {
+			if ($node->next()) {
+				$this->first = $node->next();
+				$this->first->setPrevious(null);
+			} else {
+				$this->last = $this->first = null;
+			}
+		} else if ($node == $this->last) {
+			if ($node->previous()) {
+				$this->last = $node->previous();
+				$this->last->setNext(null);
+			} else {
+				$this->last = $this->first = null;
+			}
+		} else {
+			$node->previous()->setNext($node->next());
+			$node->next()->setPrevious($node->previous());
+		}
+		$this->size--;
+	}
+	
+	public function size() {
+		return $this->size;
+	}
+	
+	public function __debugInfo() {
+		$items = [];
+		if ($this->first) {
+			$item = $this->first;
+			while($item) {
+				$items[] = ['Current: ' . $item->value(), 'Previous: ' . ($item->previous() ? $item->previous()->value() : null), 'Next: ' . ($item->next() ? $item->next()->value() : null)];
+				$item = $item->next();
+			}
+		}
+		return [
+			'first' => $this->first->value(),
+			'last' => $this->last->value(),
+			'items' => $items,
+		];
+	}
+}
